51c68b738fae4f8579a1886a3b8a9503547b460c
anilgardd <98427173+anilgardd@users.noreply.github.com>
2023-10-17 15:45:23 +0300

    Added theme toggle component and it's functionalities

--- src/app/components/themeToggle/ThemeToggle.jsx
+++ src/app/components/themeToggle/ThemeToggle.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import styles from "./themeToggle.module.css";
+import Image from "next/image";
+import { ThemeContext } from "@/app/context/ThemeContext";
 
 const ThemeToggle = () => {
-  return <div className={styles.container}>themeToggle</div>;
+  const { theme, toggle } = useContext(ThemeContext);
+  console.log(theme);
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
+      <Image src="/moon.png" alt="" width={13} height={13} />
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { right: 3, backgroundColor: "#0f172a" }
+            : { left: 3, backgroundColor: "white" }
+        }
+      ></div>
+      <Image src="/sun.png" alt="" width={13} height={13} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
